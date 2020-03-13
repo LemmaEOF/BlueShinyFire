652a218bb57067daa9ef40c849fb072782a51b7a
Vortetty <33466216+Vortetty@users.noreply.github.com>
2020-03-13 16:52:52 -0700

    Update itemsouldust.java

--- src/main/java/space/bbkr/blueshinyfire/itemsouldust.java
+++ src/main/java/space/bbkr/blueshinyfire/itemsouldust.java
@@ -1,1 +1,10 @@
-
+public class blueshinyfire implements ModInitializer{
+    // an instance of our new item
+    public static final Item SOULDUST = new Item(new Item.Settings().group(ItemGroup.MISC));
+ 
+    @Override
+    public void onInitialize()
+    {
+        Registry.register(Registry.ITEM, new Identifier("blueshinyfire", "soul_dust"), SOULDUST);
+    } 
+}

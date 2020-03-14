b3505de3673ec8c5cae98bbd1df3fbaa50dcc9fe
vortetty <vortetty@hotmail.com>
2020-03-14 11:37:51 -0700

    Added soul dust, modified mixin to accept soul dust. added sawdust added soul flint and steel to make soul fire
    todo:
     - add config option to disable new dusts

--- src/main/java/space/bbkr/blueshinyfire/mixin/MixinSoulFireBlock.java
+++ src/main/java/space/bbkr/blueshinyfire/mixin/MixinSoulFireBlock.java
@@ -1,18 +1,46 @@
 package space.bbkr.blueshinyfire.mixin;
 
+import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.SoulFireBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import space.bbkr.blueshinyfire.blueshinyfire;
 
 @Mixin(SoulFireBlock.class)
-public class MixinSoulFireBlock {
+public abstract class MixinSoulFireBlock extends AbstractFireBlock {
 	@Inject(method = "canPlaceAt", at = @At("HEAD"), cancellable = true)
 	private void injectBlueFirePersistence(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> info) {
 		info.setReturnValue(!world.getBlockState(pos.down()).isAir());
+	}
+	
+	public MixinSoulFireBlock(Settings settings, float damage) {
+		super(settings, damage);
+	}
+	
+	@Override
+	public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+		super.onEntityCollision(state, world, pos, entity);
+		
+		if (!entity.isFireImmune()) {
+			if (entity instanceof ItemEntity) {
+				ItemEntity item = (ItemEntity)entity;
+				if (item.getStack().getItem() == blueshinyfire.SAWDUST) {
+					world.setBlockState(pos, Blocks.FIRE.getDefaultState());
+				}
+				if (item.getStack().getItem() == Items.GLOWSTONE_DUST) {
+					//world.setBlockState(pos, blueshinyfire.SOULFIREBRIGHT.getDefaultState());
+				}
+			}
+		}
 	}
 }
